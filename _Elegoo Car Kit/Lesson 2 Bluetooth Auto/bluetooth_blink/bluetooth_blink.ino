e1ace62e3a2656979a5f7f46ee8abb97d81d3192
Helmut1959 <78145520+Helmut1959@users.noreply.github.com>
2023-12-09 14:50:15 +0100

    Add files via upload

---
+++ _Elegoo Car Kit/Lesson 2 Bluetooth Auto/bluetooth_blink/bluetooth_blink.ino
@@ -0,0 +1,32 @@
+//www.elegoo.com
+
+#define LED 13    //Define 13 pin for LED
+bool state = LOW; //The initial state of the function is defined as a low level
+char getstr;      //Defines a function that receives the Bluetooth character
+double test=3.1415;
+
+void setup() {
+  pinMode(LED, OUTPUT);
+  Serial.begin(9600);
+}
+
+//Control LED sub function
+void stateChange() {
+  state = !state; 
+  digitalWrite(LED, state);  
+}
+
+void loop() {
+  //The Bluetooth serial port to receive the data in the function
+  getstr = Serial.read();
+  if(getstr == 'a'){
+    stateChange();
+    String stringOne =  String(test, 3) + " " +  String(test, 3) ;  
+    Serial.print(stringOne);  
+    delay(100);
+    Serial.print(test , 3);  
+    delay(100);
+    Serial.print(String(test, 3));  
+    delay(100);
+  }
+}
