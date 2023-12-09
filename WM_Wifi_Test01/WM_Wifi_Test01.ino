0d92b874facfcc7b2ee9e820d0f5a6f1601490e9
Helmut1959 <78145520+Helmut1959@users.noreply.github.com>
2023-12-09 14:56:02 +0100

    Add files via upload

---
+++ WM_Wifi_Test01/WM_Wifi_Test01.ino
@@ -0,0 +1,15 @@
+
+#include <ESP8266WiFi.h>
+WiFiServer server(80); //Initialize the server on Port 80
+
+void setup() {
+  // put your setup code here, to run once:
+  WiFi.mode(WIFI_AP); //Our ESP8266-12E is an AccessPoint
+  WiFi.softAP("WeMosWifi", "12345678"); // Provide the (SSID, password); .
+  server.begin(); // Start the HTTP Server
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  server.stop();
+}
